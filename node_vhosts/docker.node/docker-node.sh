8b11f5e02503a8dc9ebf4a2860c44cd418363bcd
Harianto van Insulinde <harianto@pop-os.localdomain>
2019-12-06 01:08:43 +0100

    Make readable bash script

--- node_vhosts/docker.node/docker-node.sh
+++ node_vhosts/docker.node/docker-node.sh
@@ -7,7 +7,13 @@ SOCK=$DOCKER_XNMP_VHOSTS_ROOT/tmp/$DOCKER_PS_NAME.sock
 docker rm -f $DOCKER_PS_NAME
 
 docker pull harianto/api-sylo-space
-docker run -v $DOCKER_XNMP_VHOSTS_ROOT/tmp:/tmp -v $ROOT_DIR/data:/home/node/app/src/data --restart always --name $DOCKER_PS_NAME -e GID='33' -e UID='33' -e "PORT=/tmp/$DOCKER_PS_NAME.sock" -dit harianto/api-sylo-space
+docker run -v $DOCKER_XNMP_VHOSTS_ROOT/tmp:/tmp \
+ -v $ROOT_DIR/data:/home/node/app/src/data \
+ --restart always \
+ --name $DOCKER_PS_NAME \
+ -e GID='33' -e UID='33' \
+ -e "PORT=/tmp/$DOCKER_PS_NAME.sock" \
+ -dit harianto/api-sylo-space
 
 # rm $SOCK; touch $SOCK; chmod 777 $SOCK && chown www-data:www-data $SOCK
 # sleep 20
